58cd2a090e106ca5bb20dccc0ccac212a85a4020
kirimatt <wakarimasen_deshita@mail.ru>
2021-10-05 22:51:21 +0600

    Переносит личные данные в property файл.

--- src/main/java/com/company/ClassificatoryChats.java
+++ src/main/java/com/company/ClassificatoryChats.java
@@ -8,14 +8,31 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.awt.*;
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 public class ClassificatoryChats extends TelegramLongPollingBot {
 
-    private static final String USERNAME = "KirimattBot";
-    private static final String TOKEN = "1922609712:AAGEKMx2dKok9A3GXzQxKybWgBcRjldw8K8";
-    private static final String OWNER_CHAT_ID = "433128217";
-    private static final String SEAL_CHAT_ID = "767606020";
+    private static String username;
+    private static String token;
+    private static final String ownerChatId = "433128217";
+    private static final String sealChatId = "767606020";
+
+    static {
+        try (InputStream input = new FileInputStream("./config.properties")) {
+            Properties properties = new Properties();
+
+            // load a properties file
+            properties.load(input);
+
+            username = properties.getProperty("USERNAME");
+            token = properties.getProperty("TOKEN");
+
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -23,8 +40,8 @@ public class ClassificatoryChats extends TelegramLongPollingBot {
 
             try {
                 switch (update.getMessage().getChatId().toString()) {
-                    case OWNER_CHAT_ID -> IoTChat.getIoTChat().initialize(update);
-                    case SEAL_CHAT_ID -> new SealChat().initialize(update);
+                    case ownerChatId -> IoTChat.getIoTChat().initialize(update);
+                    case sealChatId -> new SealChat().initialize(update);
                     default -> new NoNameChat().initialize(update);
                 }
             } catch (TelegramApiException | IOException | AWTException e) {
@@ -36,12 +53,12 @@ public class ClassificatoryChats extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return USERNAME;
+        return username;
     }
 
     @Override
     public String getBotToken() {
-        return TOKEN;
+        return token;
     }
 
 
